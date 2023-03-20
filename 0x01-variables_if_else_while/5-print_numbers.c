5d1af58706932db1fe3302b28092241054913e12
youssef20003 <ym05914@gmail.com.com>
2023-03-20 16:21:50 -0700

    a

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - entry point
+ *disctiptin : 'checks if number is + or -'
+ *Return: always 0
+  */
+int main(void)
+{
+int n = 0;
+while (n <= 9)
+{
+printf("%d", n);
+n++;
+}
+printf("\n");
+return (0);
+}
