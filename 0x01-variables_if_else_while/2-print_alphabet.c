881be92d96bc331b288314833a619cb3dceb073b
youssef20003 <ym05914@gmail.com.com>
2023-03-20 15:40:34 -0700

    a

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - entry point
+ *disctiptin : 'checks if number is + or -'
+ *Return: always 0
+  */
+int main(void)
+{
+for (int i = 'a' ; i <= 'z' ; i++)
+{
+putchar(i);
+printf("\n");
+}
+return (0);
+}
