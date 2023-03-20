371ce3d7676496151cbecd48b73f1af22c0d6c6c
youssef20003 <ym05914@gmail.com.com>
2023-03-20 16:59:21 -0700

    a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - entry point
+ *disctiptin : 'checks if number is + or -'
+ *Return: always 0
+  */
+int main(void)
+{
+int n = 'z';
+while (n >='a')
+{
+putchar(n);
+n--;
+}
+putchar('\n');
+return (0);
+}
