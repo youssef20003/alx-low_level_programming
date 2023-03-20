67b3bf824e1798643615e4eaf87f074ce3e640e7
youssef20003 <ym05914@gmail.com.com>
2023-03-20 16:10:20 -0700

    a

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - entry point
+ *disctiptin : 'checks if number is + or -'
+ *Return: always 0
+  */
+int main(void)
+{
+int n = 'a';
+while (n <= 'z')
+{
+if (n == 'q' || n == 'e')
+{
+n++;
+}
+else
+{
+putchar(n);
+n++;
+}
+}
+return (0);
+}
