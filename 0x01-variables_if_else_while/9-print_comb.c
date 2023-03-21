a0b75fbb0b1912cfb685813efc99acc9b3ad13b7
youssef20003 <ym05914@gmail.com.com>
2023-03-20 17:37:43 -0700

    a

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,14 +11,15 @@ int main(void)
 int n = 48;
 while (n <= 57)
 {
-if(n==57){
+if(n == 57)
+{
 putchar(n);
 break;
 }
 putchar(n);
-putchar(',');
-putchar(' ');
+putchar(", ");
 n++;
 }
+putchar('\n');
 return (0);
 }
