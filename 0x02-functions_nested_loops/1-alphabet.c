1250999157b3c709a51c9773f02b904ccb3ba2a7
youssef20003 <ym05914@gmail.com.com>
2023-03-21 09:38:37 -0700

    a

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ *main - entry point
+ *disctiptin : 'checks if number is + or -'
+ *Return: always 0
+  */
+void print_alphabet(void);
+{
+int a = 'a';
+while (a <= 'z')
+{
+_putchar(a);
+a++;
+}
+_putchar("\n");
+return 0;
+}
