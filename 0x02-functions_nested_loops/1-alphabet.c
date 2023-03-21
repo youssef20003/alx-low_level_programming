89d0fd315984a49e9cd4c11d85cfdcbdcd28e8e0
youssef20003 <ym05914@gmail.com.com>
2023-03-21 09:45:25 -0700

    a

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,10 +3,9 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *main - entry point
- *disctiptin : 'checks if number is + or -'
- *Return: always 0
-  */
+ * print_alphabet - Entry point
+ * Return:0
+ */
 void print_alphabet(void)
 {
 int a = 'a';
