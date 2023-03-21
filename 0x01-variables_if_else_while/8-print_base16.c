e268654bacb29de8d38af3be360f36a4d4270e69
youssef20003 <ym05914@gmail.com.com>
2023-03-20 17:21:04 -0700

    a

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - entry point
+ *disctiptin : 'checks if number is + or -'
+ *Return: always 0
+  */
+int main(void)
+{
+for(int i = 48; i <= 57; i++)
+{
+putchar(i);  
+}
+for(int i = 'a'; i <= 'f'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
