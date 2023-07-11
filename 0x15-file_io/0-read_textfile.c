eb7831d134f0566bb3bbb6f2240385a7d1b683af
youssef20003 <ym05914@gmail.com>
2023-07-11 21:58:17 +0300

    a

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * read_textfile - ...
+ * @filename: The source file
+ * @letters: Number of letters to reads and prints
+ *
+ * Return: ...
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+    int fp;
+    char buf[READ_BUF_SIZE * 8];
+    size_t i;
+    int test;
+
+    if (!filename | !letters)
+    {
+        return (0)
+    }
+    fp = open(filename, 0_RDONLY);
+    if (fp == -1)
+    {
+        return (0);
+    }
+    i = read(fp, &buf[0], letters);
+    i = write(STDOUT_FILENO, &buf[0], i);
+    return (i);
+}
