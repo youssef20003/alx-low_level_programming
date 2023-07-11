6e321ebc1ea73bdf8d35e7059a9b6a7ebf9a80d0
youssef20003 <ym05914@gmail.com>
2023-07-12 02:37:39 +0300

    a

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,13 +14,15 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
+	{
 		return (-1);
-
+	}
 	if (text_content)
+	{
 		write(fd, text_content, _strlen(text_content));
-
+	}
 	close(fd);
 	return (1);
 }
