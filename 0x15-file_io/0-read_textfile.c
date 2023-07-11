158ddb86c8b75c2139042d9135fdc91690270a99
youssef20003 <ym05914@gmail.com>
2023-07-11 22:01:49 +0300

    a

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,21 +9,21 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fp;
-    char buf[READ_BUF_SIZE * 8];
-    size_t i;
-    int test;
+int fp;
+char buf[READ_BUF_SIZE * 8];
+size_t i;
+int test;
 
-    if (!filename | !letters)
-    {
-        return (0)
-    }
-    fp = open(filename, 0_RDONLY);
-    if (fp == -1)
-    {
-        return (0);
-    }
-    i = read(fp, &buf[0], letters);
-    i = write(STDOUT_FILENO, &buf[0], i);
-    return (i);
+if (!filename | !letters)
+{
+return (0);
+}
+fp = open(filename, 0_RDONLY);
+if (fp == -1)
+{
+return (0);
+}
+i = read(fp, &buf[0], letters);
+i = write(STDOUT_FILENO, &buf[0], i);
+return (i);
 }
