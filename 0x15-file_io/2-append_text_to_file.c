3f16fcb345ee17aa58bdf163909b6d747fc590cd
youssef20003 <ym05914@gmail.com>
2023-07-12 03:11:06 +0300

    a

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,7 +23,7 @@ int _strlen(char *s)
   *
   * Return: ...
   */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
 
